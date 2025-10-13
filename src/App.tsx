9e180946186325f92f4f817f8f0f254feb9dc2d0
Callme-Red <robertdiruz05@gmail.com>
2025-10-13 09:09:48 -0600

    Arreglando las rutas de customer y estableciendo las del modulo supplier

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { useAuth } from './api/auth-provider';
 import Spinner from './components/spinner';
 import Login from './pages/auth/login';
 import Customer from './pages/customer/customer';
+import AddSupplier from './pages/supplier/add-supplier';
+import Supplier from './pages/supplier/supplier';
+import AddCustomer from './pages/customer/add-customer';
 
 const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
   const { user, loading } = useAuth();
@@ -27,6 +30,22 @@ function App() {
     {
       path: '/customers',
       element: <ProtectedRoute element={<Customer />} />
+    },
+    {
+      path: '/suppliers/add/:supplierId?',
+      element: <ProtectedRoute element={<AddSupplier />} />
+    },
+    {
+      path: '/suppliers',
+      element: <ProtectedRoute element={<Supplier />} />
+    },
+    {
+      path: '/customers/add',
+      element: <ProtectedRoute element={<AddCustomer />} />
+    },
+    {
+      path: 'customers/add/:customerId?',
+      element: <ProtectedRoute element={<AddCustomer />} />
     },
   ]);
 
